3809f458a0d57d81236496744be52d462d6b59a2
Shrey Puranik <shrey@shreypuranik.co.uk>
2024-03-23 22:26:37 +0000

    test: Adding test

---
+++ converter_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestConverterBehavesAsExpected(t *testing.T) {
+	a := assert.New(t)
+
+	csvFilePath := "./mockcsv_data.csv"
+	jsonFilePath := "./mockjson_data.json"
+
+	Convert(csvFilePath, jsonFilePath)
+
+	jsonFile, _ := os.Open(jsonFilePath)
+	byteResult, _ := io.ReadAll(jsonFile)
+
+	var songs []Song
+	json.Unmarshal(byteResult, &songs)
+
+	fmt.Println(songs)
+
+	a.Equal(3, len(songs), "3 songs found in converted file")
+}
